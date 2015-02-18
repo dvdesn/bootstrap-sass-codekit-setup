7e0ee86f755f4480fae6e0fb405c655ebe92d473
davide <davide@Davide-Senatras-MacBook-Pro.local>
2015-02-18 15:58:16 +0100

    Clean State

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bootstrap3+Sass+Codekit SetUp</title>
+	<link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyle.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="jumbotron">
+			<h1>Bootstrap + Sass + Codekit</h1><br>
+			<div class="btn btn-primary">Just a btn</div> 
+		</div>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore vero dolorem laudantium perferendis, quis quaerat corporis blanditiis quas, minima veniam iure placeat, unde itaque distinctio ratione aut libero sunt ipsam!
+		</p>
+		
+	</div><!-- container-fluid -->
+
+<script src="_/js/myscript-min.js"></script>
+<script>
+
+</script>
+</body>
+</html>
